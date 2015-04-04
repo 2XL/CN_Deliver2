e5df5e085cc0f632a00e6c1e08853ef60e353e40
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-04-04 04:40:43 +0200

    Doing Louvian, TODO: render directory of sourcefiles, with select, TODO: add render button, TODO: parse .net to json... ordered preference

--- js/script.js
+++ js/script.js
@@ -5,3 +5,41 @@
  */
 
 
+function pajekToJSON(dir) {
+    var json;
+
+    // dir is the path of the file.net
+
+
+
+
+    // format
+    // param -> values
+
+
+}
+
+// haha aixo o Jquery en una linea...
+function loadJSON(path, callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', path, true);
+    xobj.onreadystatechange = function () {
+	if (xobj.readyState === 4 && xobj.status === 200) {
+// .open will NOT return a value but simply returns undefined in async mode so use a callback
+	    callback(xobj.responseText);
+	}
+    }
+    xobj.send(null);
+}
+
+function isJson(str) {
+    try {
+	var data = JSON.parse(str);
+    } catch (e) {
+	return {json: false, data: str};
+    }
+    return {json: true, data: data};
+}
+ 
